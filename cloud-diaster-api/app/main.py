e1adc4f89dd72f318d279d5f542e592c05679bad
iseyf <c4033962@newcastle.ac.uk>
2025-07-03 12:24:51 +0100

    Added websocket endpoints for live location/tracking of my rescue ,shelter teams etc.

--- cloud-diaster-api/app/main.py
+++ cloud-diaster-api/app/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from app.routes import router
+from app.websocket import router as websocket_router
 
 app = FastAPI()
 
 app.include_router(router)
+app.include_router(websocket_router)
 @app.get("/")
 async def root():
     return {"status": "Diaster Response API is live"}
---
+++ cloud-diaster-api/app/websocket.py
@@ -0,0 +1,31 @@
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect
+
+router = APIRouter()
+
+active_connections: list[WebSocket] = []            #List to track who is currently connected
+
+
+#Connecting clients
+
+async def connect_client(websocket: WebSocket):
+    await websocket.accept()                        #Awaiting their connection
+    active_connections.append(websocket)            #Add to list of active connections
+
+def disconnect_client(websocket: WebSocket):            #If user disconnects remove from list
+    active_connections.remove(websocket)
+
+async def broadcast_msg(message: str):                      #Send message to every connected client
+    for connection in active_connections:
+        await connection.send_text(message)
+
+
+@router.websocket("/ws/locations")
+async def websocket_locations(websocket: WebSocket):
+    await connect_client(websocket)                         #wait for user to connect, and add to list
+    try:
+        while True:
+            data = await websocket.receive_text()            #Wait till we recieve text , then broadcast it
+            await broadcast_msg(data)
+    except WebSocketDisconnect as e:                            #If we disconnect, remove them from websocket
+        print(e)
+        disconnect_client(websocket)
